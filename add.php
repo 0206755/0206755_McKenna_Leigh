57c088932e8fcb1d4177c9242a71be5aa47115df
0206755 <0206755@rgu.ac.uk>
2016-05-04 11:41:48 +0100

    Commit

---
+++ add.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<main>
+
+
+    <div class="grid-container" id="no-padding">
+        <header>
+
+            <h1>myBlog</h1>
+            <h2>because the internet needs to know what I think</h2>
+            <nav>
+                <ul>
+                    <li><a href="blog.php">All Blog Items</a></li>
+                    <li><a href="blog.php">Work Items</a></li>
+                    <li><a href="blog.php">University Items</a></li>
+                    <li><a href="blog.php">Family Items</a></li>
+                    <li><a href="add.php">Insert a Blog Item</a></li>
+                </ul>
+
+            </nav>
+            </header>
+
+        <div id="main-section">
+        <div class="grid-66" id="margin-auto">
+            <h1>Add Blog Item</h1>
+            <section class="container" id="contentLeft">
+            <form>
+            <label id="label-margin">Entry Title:</label> <input type="text" name="entry_title" required><br />
+                <label id="label-margin">Entry Summary:</label> <textarea name="entry_summary" required></textarea><br />
+                <label id="label-margin">Category:</label>
+                <select name="entry_category" required="required">
+                    <option value="" >Select Category</option>
+                <option value="Work">Work</option>
+                <option value="University">University</option>
+                <option value="Family">Family</option>
+
+            </select><br/>
+                <label id="label-margin">Submitted By:</label> <input type="text" name="entry_title"><br />
+                <input type="submit" name="submit" value="submit">
+
+            </form>
+            </section>
+        </div>
+
+
+
+
+        </div>
+
+
+
+    </div>
+    <footer>Designed by Leigh McKenna, 2016</footer>
+</main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<main>
+
+
+    <div class="grid-container" id="no-padding">
+        <header>
+
+            <h1>myBlog</h1>
+            <h2>because the internet needs to know what I think</h2>
+            <nav>
+                <ul>
+                    <li><a href="blog.php">All Blog Items</a></li>
+                    <li><a href="blog.php">Work Items</a></li>
+                    <li><a href="blog.php">University Items</a></li>
+                    <li><a href="blog.php">Family Items</a></li>
+                    <li><a href="add.php">Insert a Blog Item</a></li>
+                </ul>
+
+            </nav>
+            </header>
+
+        <div id="main-section">
+        <div class="grid-66">
+            <section class="container" id="contentLeft">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+            </section>
+        </div>
+
+
+        <div class="grid-33">
+            <aside id="contentRight">
+                <img src="images/blog.png" class="blogFoto" alt="myBlog" title="myBlog">
+            </aside>
+        </div>
+
+        </div>
+
+
+
+    </div>
+    <footer>Designed by Leigh McKenna, 2016</footer>
+</main>
+</body>
+</html>
