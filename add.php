0689201de3ebd67d5d53890cb39ada0487f46358
0206755 <0206755@rgu.ac.uk>
2016-05-04 12:23:42 +0100

    Commit

--- add.php
+++ add.php
@@ -18,11 +18,11 @@
             <h2>because the internet needs to know what I think</h2>
             <nav>
                 <ul>
-                    <li><a href="blog.php">All Blog Items</a></li>
-                    <li><a href="blog.php">Work Items</a></li>
-                    <li><a href="blog.php">University Items</a></li>
-                    <li><a href="blog.php">Family Items</a></li>
-                    <li><a href="add.php">Insert a Blog Item</a></li>
+                    <li><a href="blog.html">All Blog Items</a></li>
+                    <li><a href="blog.html">Work Items</a></li>
+                    <li><a href="blog.html">University Items</a></li>
+                    <li><a href="blog.html">Family Items</a></li>
+                    <li><a href="add.html">Insert a Blog Item</a></li>
                 </ul>
 
             </nav>
@@ -32,9 +32,9 @@
         <div class="grid-66" id="margin-auto">
             <h1>Add Blog Item</h1>
             <section class="container" id="contentLeft">
-            <?php
-            if ($SERVER['REQUEST_METHOD'] === 'GET') { ?>
-                <form action="add.php" method="post">
+                <?php
+                if ($SERVER['REQUEST_METHOD'] === 'GET') { ?>
+            <form>
             <label id="label-margin">Entry Title:</label> <input type="text" name="entry_title" required><br />
                 <label id="label-margin">Entry Summary:</label> <textarea name="entry_summary" required></textarea><br />
                 <label id="label-margin">Category:</label>
@@ -49,42 +49,6 @@
                 <input type="submit" name="submit" value="submit">
 
             </form>
-           <?php }
-            elseif ($SERVER[REQUEST_METHOD] === 'POST') {
-
-                $entryTitle = $_GET['entryTitle'];
-                $entrySummary = $_GET['entrySummary'];
-                $category = $_GET['category'];
-                $submitter = $_GET['submitter'];
-                }
-
-            define ('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-            define ('DB_USERNAME', 'b9b4f1f2913587');
-            define ('DB_PASSWORD', '81a7c479');
-            define ('DB_DATABASE','acsm_3a7bd64b8322cf2');
-            $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-            if (mysqli_connect_errno()) {
-                printf("Connect failed: %s\n", mysqli_connect_error());
-                exit();
-            }
-
-            else{
-
-                $sql = "INSERT INTO blogView (entryTitle,entrySummary,category,submitter)
-                VALUES ($entryTitle,$entrySummary,$category,$submitter)";
-
-                echo "New blog article added successfully";
-
-                }
-
-                $conn->close();
-
-                Header("Location: blog.php");
-
-            else {
-                Header("Location: index.php");
-                }
-?>
             </section>
         </div>
 
